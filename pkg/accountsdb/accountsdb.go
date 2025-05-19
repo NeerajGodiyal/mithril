98cdc6a865337033d5f83556fb128c5de399d1f1
smcio <shaun@darkwaves.io>
2025-05-19 00:30:13 +0000

    cleanup comments

--- pkg/accountsdb/accountsdb.go
+++ pkg/accountsdb/accountsdb.go
@@ -204,19 +204,6 @@ func (accountsDb *AccountsDb) GetAccount(slot uint64, pubkey solana.PublicKey) (
 var voteAcct = solana.MustPublicKeyFromBase58("Vote111111111111111111111111111111111111111")
 
 func (accountsDb *AccountsDb) StoreAccounts(accts []*accounts.Account, slot uint64) error {
-	/*fileId := accountsDb.LargestFileId.Add(1)
-
-	appendVecFileName := fmt.Sprintf("%s/%d.%d", accountsDb.AcctsDir, slot, fileId)
-	appendVecFile, err := os.OpenFile(appendVecFileName, os.O_RDWR|os.O_CREATE, 0666)
-	if err != nil {
-		mlog.Log.Debugf("unable to open appendvec file %s for writing to accountsdb", appendVecFileName)
-		return err
-	}
-	defer appendVecFile.Close()*/
-
-	//appendVecAcctsBuf := new(bytes.Buffer)
-	//writer := new(bytes.Buffer)
-
 	go accountsDb.storeAccountsInternal(accts, slot)
 
 	for _, acct := range accts {
@@ -228,42 +215,7 @@ func (accountsDb *AccountsDb) StoreAccounts(accts []*accounts.Account, slot uint
 		} else {
 			accountsDb.CommonAcctsCache.Set(acct.Key, acct)
 		}
-
-		// create index entry, encode it and write it to the index kv store
-		// offset field is specified as the current num of bytes written to the appendvec buffer.
-		/*writer.Reset()
-		encoder := bin.NewBinEncoder(writer)
-
-		indexEntry := AccountIndexEntry{Slot: slot, FileId: fileId, Offset: uint64(appendVecAcctsBuf.Len())}
-
-		err = indexEntry.MarshalWithEncoder(encoder)
-		if err != nil {
-			mlog.Log.Debugf("error marshaling in Set on accountsdb for pubkey %s", acct.Key)
-			return err
-		}
-
-		err = accountsDb.Index.Set(acct.Key[:], writer.Bytes())
-		if err != nil {
-			panic(fmt.Sprintf("unable to add acct for %s to acctsdb", acct.Key))
-		}
-
-		// marshal up the account as an appendvec style account and write it to the buffer
-		appendVecAcct := AppendVecAccount{DataLen: uint64(len(acct.Data)), Pubkey: acct.Key, Lamports: acct.Lamports,
-			RentEpoch: acct.RentEpoch, Owner: acct.Owner, Executable: acct.Executable, Data: acct.Data}
-
-		err = appendVecAcct.Marshal(appendVecAcctsBuf)
-		if err != nil {
-			return err
-		}*/
 	}
-
-	// write the appendvecs data into the file
-	/*n, err := appendVecFile.Write(appendVecAcctsBuf.Bytes())
-	if err != nil {
-		return err
-	} else if n != appendVecAcctsBuf.Len() {
-		return fmt.Errorf("only wrote %d appendvec account bytes, rather than %d", n, appendVecAcctsBuf.Len())
-	}*/
 
 	return nil
 }
