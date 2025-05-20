848be41ee305edf9f2a013fa93528edd2f032cdd
Palmer Lao <palmer.lao@gmail.com>
2025-05-19 20:45:47 -0700

    Remove mlog data race

--- pkg/mlog/mlog.go
+++ pkg/mlog/mlog.go
@@ -2,18 +2,19 @@ package mlog
 
 import (
 	"fmt"
+	"sync/atomic"
 
 	"k8s.io/klog/v2"
 )
 
 type logger struct {
-	enableVerbose bool
+	enableVerbose *atomic.Bool
 }
 
-var Log = logger{}
+var Log = logger{&atomic.Bool{}}
 
 func (log *logger) Debugf(format string, args ...interface{}) {
-	if log.enableVerbose {
+	if log.enableVerbose.Load() {
 		klog.InfoDepth(1, fmt.Sprintf(format, args...))
 	}
 }
@@ -27,9 +28,9 @@ func (log *logger) Errorf(format string, args ...interface{}) {
 }
 
 func (log *logger) EnableInfLogging() {
-	log.enableVerbose = true
+	log.enableVerbose.Store(true)
 }
 
 func (log *logger) DisableInfLogging() {
-	log.enableVerbose = false
+	log.enableVerbose.Store(false)
 }
