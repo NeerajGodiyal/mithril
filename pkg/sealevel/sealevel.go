85a82cf02378ce161f7901359dc43f836e66a7a2
smcio <shaun@darkwaves.io>
2025-06-10 13:32:28 +0000

    sealevel: fix corner case in InstructionAcctsFromAccountMetas

--- pkg/sealevel/sealevel.go
+++ pkg/sealevel/sealevel.go
@@ -95,3 +95,38 @@ func IsNativeProgram(pubkey solana.PublicKey) bool {
 		return false
 	}
 }
+
+func InstructionAcctsFromAccountMetas(instrAcctMetas []AccountMeta, txAccounts TransactionAccounts) []InstructionAccount {
+	var instrAccts []InstructionAccount
+
+	for instrAcctIdx, accountMeta := range instrAcctMetas {
+		idxInTx := -1
+		for pos, acct := range txAccounts.Accounts {
+			a := *acct
+			if a.Key == accountMeta.Pubkey {
+				idxInTx = pos
+				break
+			}
+		}
+		if idxInTx == -1 {
+			idxInTx = len(txAccounts.Accounts)
+		}
+
+		accts := instrAccts[:instrAcctIdx]
+		idxInCallee := -1
+		for pos, instrAcct := range accts {
+			if instrAcct.IndexInTransaction == uint64(idxInTx) {
+				idxInCallee = pos
+				break
+			}
+		}
+		if idxInCallee == -1 {
+			idxInCallee = instrAcctIdx
+		}
+
+		newInstrAcct := InstructionAccount{IndexInTransaction: uint64(idxInTx), IndexInCaller: uint64(idxInTx), IndexInCallee: uint64(idxInCallee), IsSigner: accountMeta.IsSigner, IsWritable: accountMeta.IsWritable}
+		instrAccts = append(instrAccts, newInstrAcct)
+	}
+
+	return instrAccts
+}
