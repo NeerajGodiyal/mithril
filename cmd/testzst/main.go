ddbd0770a70b4ca013f11a37bfa675195ed09156
palmerlao <palmerlao@users.noreply.github.com>
2026-03-03 21:31:52 -0800

    Update cmd/testzst to check disk throughput

--- cmd/testzst/main.go
+++ cmd/testzst/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"io"
 	"os"
@@ -13,6 +14,7 @@ import (
 func main() {
 	zstdFilename := flag.String("zstd-file", "", "Path to zstd compressed file")
 	concurrency := flag.Int("zstd-decoder-concurrency", 1, "Number of decoder goroutines")
+	outputFile := flag.String("output", "", "Optional output file to write decompressed data (measures decompress+write throughput)")
 
 	flag.Parse()
 
@@ -32,7 +34,20 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = zstdReader.WriteTo(io.Discard)
+	var dst io.Writer = io.Discard
+	if *outputFile != "" {
+		f, err := os.Create(*outputFile)
+		if err != nil {
+			mlog.Log.Errorf("creating output file %s: %v", *outputFile, err)
+			os.Exit(1)
+		}
+		defer f.Close()
+		bw := bufio.NewWriterSize(f, 4*1024*1024)
+		defer bw.Flush()
+		dst = bw
+	}
+
+	_, err = zstdReader.WriteTo(dst)
 	if err != nil {
 		mlog.Log.Errorf("WriteTo: %v", err)
 		os.Exit(1)
