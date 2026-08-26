ae8bc0c4c14eb94871591c62d4b918858e64dcaf
Neeraj Godiyal <neerajgodiyal250@gmail.com>
2026-08-26 19:35:29 +0530

    test: preserve stake index state across interrupted writes

---
+++ pkg/global/stake_index_durability_test.go
@@ -0,0 +1,54 @@
+package global
+
+import (
+	"encoding/binary"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/Overclock-Validator/mithril/pkg/accountsdb"
+	"github.com/gagliardetto/solana-go"
+	"github.com/stretchr/testify/require"
+)
+
+func resetStakeIndexTestState() {
+	instance.pendingStakeMutex.Lock()
+	defer instance.pendingStakeMutex.Unlock()
+	instance.pendingStakeBySlot = nil
+	instance.cachedStakeEntries = nil
+	instance.entriesFlushedSinceCompact = 0
+}
+
+func TestFlushPendingStakePubkeysThroughRetainsEntriesOnOpenFailure(t *testing.T) {
+	resetStakeIndexTestState()
+	defer resetStakeIndexTestState()
+
+	pubkey := solana.PublicKey{1}
+	EnqueuePendingStakePubkey(7, pubkey)
+	_, err := FlushPendingStakePubkeysThrough(filepath.Join(t.TempDir(), "missing"), 7)
+	require.Error(t, err)
+	require.Equal(t, []accountsdb.StakeIndexEntry{{Pubkey: pubkey}}, PendingStakeEntriesSnapshot())
+}
+
+func TestLoadStakePubkeyIndexRecoversIncompleteFinalRecord(t *testing.T) {
+	resetStakeIndexTestState()
+	defer resetStakeIndexTestState()
+
+	dir := t.TempDir()
+	path := filepath.Join(dir, StakePubkeyIndexFileName)
+	pubkey := solana.PublicKey{2}
+	data := make([]byte, 8+accountsdb.StakeIndexRecordSize+7)
+	copy(data[:4], accountsdb.StakeIndexMagic[:])
+	binary.LittleEndian.PutUint32(data[4:8], accountsdb.StakeIndexVersion)
+	copy(data[8:40], pubkey[:])
+	binary.LittleEndian.PutUint64(data[40:48], 11)
+	binary.LittleEndian.PutUint64(data[48:56], 22)
+	require.NoError(t, os.WriteFile(path, data, 0o644))
+
+	entries, err := LoadStakePubkeyIndex(dir)
+	require.NoError(t, err)
+	require.Equal(t, []accountsdb.StakeIndexEntry{{Pubkey: pubkey, FileId: 11, Offset: 22}}, entries)
+	info, err := os.Stat(path)
+	require.NoError(t, err)
+	require.Equal(t, int64(8+accountsdb.StakeIndexRecordSize), info.Size())
+}
